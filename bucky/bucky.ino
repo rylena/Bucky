88cc3a379e086fd990b333c024ea90aebc82e3c2
rylena <rylen.anil@gmail.com>
2025-03-16 00:20:11 +0400

    allowing user to give commands wtih serial terminal

--- bucky/bucky.ino
+++ bucky/bucky.ino
@@ -1,6 +1,6 @@
 #include <BleKeyboard.h>
 
-BleKeyboard bleKeyboard("ESP32 Auto Keystroker");
+BleKeyboard bleKeyboard("ESP32 Ducky");
 
 void setup() {
     Serial.begin(115200);
@@ -10,8 +10,51 @@ void setup() {
 
 void loop() {
     if (bleKeyboard.isConnected()) {
-        Serial.println("Sending: helloworld");
-        bleKeyboard.print(String("helloworld").c_str());  // Convert to const char*
-        delay(5000);  // Wait 5 seconds before sending again
+        if (Serial.available()) {
+            String command = Serial.readStringUntil('\n');  // Read line from Serial
+            command.trim();  // Remove extra spaces/newlines
+            
+            if (command.startsWith("STRING ")) {  // Typing text
+                String text = command.substring(7);
+                bleKeyboard.print(text);
+            } 
+            else if (command == "ENTER") {
+                bleKeyboard.write(KEY_RETURN);
+            }
+            else if (command.startsWith("DELAY ")) {  // Pause execution
+                int delayTime = command.substring(6).toInt();
+                delay(delayTime);
+            }
+            else if (command == "CTRL ALT DELETE") {  // Special key combo
+                bleKeyboard.press(KEY_LEFT_CTRL);
+                bleKeyboard.press(KEY_LEFT_ALT);
+                bleKeyboard.press(KEY_DELETE);
+                delay(100);
+                bleKeyboard.releaseAll();
+            }
+            else if (command.startsWith("CTRL ")) {  // CTRL + <Key>
+                char key = command[5];
+                bleKeyboard.press(KEY_LEFT_CTRL);
+                bleKeyboard.press(key);
+                delay(100);
+                bleKeyboard.releaseAll();
+            }
+            else if (command.startsWith("ALT ")) {  // ALT + <Key>
+                char key = command[4];
+                bleKeyboard.press(KEY_LEFT_ALT);
+                bleKeyboard.press(key);
+                delay(100);
+                bleKeyboard.releaseAll();
+            }
+            else if (command.startsWith("SHIFT ")) {  // SHIFT + <Key>
+                char key = command[6];
+                bleKeyboard.press(KEY_LEFT_SHIFT);
+                bleKeyboard.press(key);
+                delay(100);
+                bleKeyboard.releaseAll();
+            }
+
+            Serial.println("Executed: " + command);
+        }
     }
 }
